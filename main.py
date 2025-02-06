b644e93794a7494589f2c6d092c45fd75f1a3e30
Lucas Rojas <lucas.rojas@live.com.ar>
2025-02-06 23:12:28 +0000

    Restore pagination to main.py and rename howToSave images

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages.sidemenu import pg
+
 # st.markdown('# Main Page') # Queda como un titulo para todo
 
 # Logica de logueo
@@ -19,6 +19,7 @@ from pages.sidemenu import pg
 # login_page = st.Page(login, title="Log in", icon=":material/login:")
 # logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 
+
 introduction = st.Page('pages/introduction.py',
                        title='Introduccion', default=True)
 commands = st.Page(
@@ -26,14 +27,20 @@ commands = st.Page(
 )
 resources = st.Page("pages/resources.py", title="Recursos",
                     icon=":material/favorite:")
-# Cheque en el estado de la sesion si el flag de logged_in es true
-# if st.session_state.logged_in:
+
+howToSave = st.Page("pages/howToSave.py", title="Como guardar los cambios",
+                    icon=":material/favorite:")
 
 # Funciona como router, desde aca se renderizan las paginas
 
-# Con esto configuro a mano las paginas pudiendo customizar el titulo e icon
+# Con esto configuro a mano las paginas pudiendo customizar el titulo e icono
+pg = st.navigation(
+    {
+        "": [introduction],
+        # "Account": [logout_page],
+        "Recursos": [resources, howToSave],
+        "Commands": [commands]
+    }
+)
+
 pg.run()
-# else:
-#     pg = st.navigation([login_page])
-
-
--- pages/howToSave.py
+++ pages/howToSave.py
@@ -1,21 +1,22 @@
 import streamlit as st
+image_prefix="images/howToSave/"
 
 st.markdown('''
 
 # Como guardar los cambios?
 Cuando hayamos hecho un cambio vamos a poder ver en la barra lateral izquierda el siguiente icono. El numero que tenga va variar en base a la cantidad de archivos que hayamos cambiado.
  ''')
-st.image("images/Screenshot 2025-02-06 195109.png")
+st.image(f"{image_prefix}changes_icon.png")
 
 st.markdown('''
 Al hacer click, se va a abrir el controlador de cambios 
 ''')
 
-st.image("images/Screenshot 2025-02-06 195355.png")
+st.image(f"{image_prefix}changes_list.png")
 st.text("Al posicion el raton a la altura del texto Changes va aparecer una serie de iconos, al hacer click en le icono de mas '+' va a seleccionar todos los archivos para poder generar un 'commit' ")
-st.image("images/Screenshot 2025-02-06 195436.png")
+st.image(f"{image_prefix}changes_list_plus_sign.png")
 st.text("Una vez hecho esto, colocamos un mensaje corto explicando los cambios que se hicieron en el campo de texto que esta arriba y confirmamos haciendo click en el boton que dice 'Commit' ")
-st.image("images/Screenshot 2025-02-06 195824.png")
+st.image(f"{image_prefix}staged_changes_message.png")
 st.text("Generado el commit, vamos a ver que la lista desaparece y el boton cambia a 'Sync Changes'")
-st.image('images/Screenshot 2025-02-06 200131.png')
-st.text('Al clickearlo va a enviar los cambios al repositorio')
+st.image(f"{image_prefix}changes_ready_push.png")
+st.text('Al clickearlo va a enviar los cambios al repositorio. Luedo de unos minutos el sitio va estar actualizado')
